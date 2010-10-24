851ebcc827324768e33f8a6ea0c776202f902751
philm <philm@chakra-project.org>
2010-10-24 17:15:47 +0000

    update pacman to 3.4.1

--- aufs2/create-tarball.sh
+++ aufs2/create-tarball.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-AUFS2VERSION="-33"
-KERNELVERSION=2.6.33
-GITSNAPSHOT=20100420
+AUFS2VERSION="-36"
+KERNELVERSION=2.6.36
+GITSNAPSHOT=20101021
 # aufs2 (no -xx) for the latest -rc version.
 git clone http://git.c3sl.ufpr.br/pub/scm/aufs/aufs2-standalone.git aufs2-standalone.git
 cd aufs2-standalone.git
-git checkout origin/aufs2${AUFS2VERSION}
+#git checkout origin/aufs2${AUFS2VERSION}
+git checkout origin/aufs2
 #*** apply "aufs2-base.patch" and "aufs2-standalone.patch" to your kernel source files.
 cd ..
 rm -rf aufs2-${KERNELVERSIN}_${GITSNAPSHOT}
