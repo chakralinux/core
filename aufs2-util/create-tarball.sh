61ebe2253975162537dbf4fcfa98d41d45e5b9fe
philm <philm@chakra-project.org>
2011-05-19 09:26:14 +0000

    kernel26: bump to 2.6.39 kernel26 modules: bump to 2.6.39 series

--- aufs2-util/create-tarball.sh
+++ aufs2-util/create-tarball.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #AUFS2VERSION=""
 #KERNELVERSION=2.6.35
-GITSNAPSHOT=20101025
+GITSNAPSHOT=20110519
 # aufs2 (no -xx) for the latest -rc version.
 rm -R aufs2-util.git
 git clone http://git.c3sl.ufpr.br/pub/scm/aufs/aufs2-util.git aufs2-util.git
