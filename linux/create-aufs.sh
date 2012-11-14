f324c831db1e139b4f0db19734dc62b4e5c3d62d
abveritas <abveritas@chakra-project.org>
2012-11-14 00:09:55 +0000

    linux update for move to /usr/lib, mkinitcpio trial to get to /usr/lib, not succesful yet

--- linux/create-aufs.sh
+++ linux/create-aufs.sh
@@ -5,7 +5,7 @@ rm -Rf aufs3-standalone
 git clone git://aufs.git.sourceforge.net/gitroot/aufs/aufs3-standalone.git
 cd aufs3-standalone
 git branch -r
-git checkout origin/aufs3.5
+git checkout origin/aufs3.6
 
 # modify what you want
 
@@ -15,16 +15,16 @@ git checkout origin/aufs3.5
 rm -v $(find . -type f -name '*.orig')
 grep -qse 'EXPORT_SYMBOL(' aufs3-standalone.patch && \
 sed -i-old -e 's|EXPORT_SYMBOL(|EXPORT_SYMBOL_GPL(|' aufs3-standalone.patch
-rm -rf ../tmp/linux-3.5
-mkdir -p ../tmp/linux-3.5
-cp -a fs ../tmp/linux-3.5
-cp -a include ../tmp/linux-3.5
-cp -a Documentation ../tmp/linux-3.5
-rm ../tmp/linux-3.5/include/linux/Kbuild
+rm -rf ../tmp/linux-3.6
+mkdir -p ../tmp/linux-3.6
+cp -a fs ../tmp/linux-3.6
+cp -a include ../tmp/linux-3.6
+cp -a Documentation ../tmp/linux-3.6
+rm ../tmp/linux-3.6/include/linux/Kbuild
 cd ../tmp
-diff -Naur null linux-3.5  | filterdiff | \
-sed -e 's|null\(/include/linux/Kbuild\)|linux-3.5-old\1|;s|^--- null.*|--- /dev/null|;\|linux-3.5/include/linux/Kbuild|,${\|@@ -0,0 +1 @@|,$d}' \
-| bzip2 > aufs$(sed -ne 's|#define.*AUFS_VERSION.*"\(.*\)"|\1|p'  linux-3.5/include/linux/aufs_type.h).patch.bz2
+diff -Naur null linux-3.6  | filterdiff | \
+sed -e 's|null\(/include/linux/Kbuild\)|linux-3.6-old\1|;s|^--- null.*|--- /dev/null|;\|linux-3.6/include/linux/Kbuild|,${\|@@ -0,0 +1 @@|,$d}' \
+| bzip2 > aufs$(sed -ne 's|#define.*AUFS_VERSION.*"\(.*\)"|\1|p'  linux-3.6/include/linux/aufs_type.h).patch.bz2
 mv *.bz2 $OLDPWD
 cd $OLDPWD
 mv *patch* ..
