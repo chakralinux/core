981d923818c17e0b8cb5a294c2f527f3d2c3f976
abveritas <abveritas@chakra-project.org>
2013-01-14 21:42:19 +0000

    prep linux for 3.7

--- linux/create-aufs.sh
+++ linux/create-aufs.sh
@@ -5,7 +5,7 @@ rm -Rf aufs3-standalone
 git clone git://aufs.git.sourceforge.net/gitroot/aufs/aufs3-standalone.git
 cd aufs3-standalone
 git branch -r
-git checkout origin/aufs3.6
+git checkout origin/aufs3.7
 
 # modify what you want
 
@@ -15,16 +15,16 @@ git checkout origin/aufs3.6
 rm -v $(find . -type f -name '*.orig')
 grep -qse 'EXPORT_SYMBOL(' aufs3-standalone.patch && \
 sed -i-old -e 's|EXPORT_SYMBOL(|EXPORT_SYMBOL_GPL(|' aufs3-standalone.patch
-rm -rf ../tmp/linux-3.6
-mkdir -p ../tmp/linux-3.6
-cp -a fs ../tmp/linux-3.6
-cp -a include ../tmp/linux-3.6
-cp -a Documentation ../tmp/linux-3.6
-rm ../tmp/linux-3.6/include/linux/Kbuild
+rm -rf ../tmp/linux-3.7
+mkdir -p ../tmp/linux-3.7
+cp -a fs ../tmp/linux-3.7
+cp -a include ../tmp/linux-3.7
+cp -a Documentation ../tmp/linux-3.7
+rm ../tmp/linux-3.7/include/uapi/linux/Kbuild
 cd ../tmp
-diff -Naur null linux-3.6  | filterdiff | \
-sed -e 's|null\(/include/linux/Kbuild\)|linux-3.6-old\1|;s|^--- null.*|--- /dev/null|;\|linux-3.6/include/linux/Kbuild|,${\|@@ -0,0 +1 @@|,$d}' \
-| bzip2 > aufs$(sed -ne 's|#define.*AUFS_VERSION.*"\(.*\)"|\1|p'  linux-3.6/include/linux/aufs_type.h).patch.bz2
+diff -Naur null linux-3.7  | filterdiff | \
+sed -e 's|null\(/include/uapi/linux/Kbuild\)|linux-3.7-old\1|;s|^--- null.*|--- /dev/null|;\|linux-3.7/include/uapi/linux/Kbuild|,${\|@@ -0,0 +1 @@|,$d}' \
+| bzip2 > aufs$(sed -ne 's|#define.*AUFS_VERSION.*"\(.*\)"|\1|p'  linux-3.7/include/linux/aufs_type.h).patch.bz2
 mv *.bz2 $OLDPWD
 cd $OLDPWD
 mv *patch* ..
